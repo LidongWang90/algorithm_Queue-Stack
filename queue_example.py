b24637cb8d0b60c3ff6b37966f364de1f5752b9e
Lidong90 <98975055+Lidong90@users.noreply.github.com>
2023-05-13 22:57:09 -0400

    5/13new

--- queue_example.py
+++ queue_example.py
@@ -3,20 +3,32 @@ from collections import deque
 
 class Solution:
     def countStudents(students, sandwiches):
-        # use deque
-        dst = deque(students)
-        dsa = deque(sandwiches)
-        count = 0  # taking a counter to count uneaten students.
-        while count < len(dst):
-            if dst[0] == dsa[0]:
-                dsa.popleft()
-                dst.popleft()
+        # use deque:
+        # dst = deque(students)
+        # dsa = deque(sandwiches)
+        # count = 0  # taking a counter to count uneaten students.
+        # while count < len(dst):
+        #     if dst[0] == dsa[0]:
+        #         dsa.popleft()
+        #         dst.popleft()
+        #         count = 0
+        #     else:
+        #         dst.append(dst.popleft())
+        #         count += 1
+
+        # return len(dst)
+
+        # without deque:
+        count = 0
+        while count < len(students):
+            if students[0] == sandwiches[0]:
+                students.pop(0)
+                sandwiches.pop(0)
                 count = 0
             else:
-                dst.append(dst.popleft())
+                students.append(students.pop(0))
                 count += 1
-
-        return len(dst)
+        return len(students)
 
 
 students = [1, 1, 1, 0, 0, 1]
